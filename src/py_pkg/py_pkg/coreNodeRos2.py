80412ad8789e62fe804835d0e730683894e1299d
raybeak <ray.beak@outlook.com>
2026-06-19 06:33:55 +0900

    moved to ros2 node

--- src/py_pkg/py_pkg/coreNodeRos2.py
+++ src/py_pkg/py_pkg/coreNodeRos2.py
@@ -26,3 +26,24 @@ from ackermanCalculator:
     - gets the actual ackerman steering to control the vehicle to reach the target waypoint and curvature
 """
 #!/usr/bin/env python3
+import trajectoryManager
+testLocationStr="./wp_optimal_OX.csv"
+#"./wp_optimal_XO.csv"
+#"./wp_optimal_XX.csv"
+#'./wp_optimal_OO.csv'
+#"./wp_amcl-2026-06-11-09-12-07-clean_copy.csv"
+class Dummy:
+    pass
+
+vp = Dummy()
+vp.max_accel = 2.0
+vp.max_decel = -2.0
+
+vp.max_speed = 5.0
+
+vp.min_speed = 0.5
+
+tm = trajectoryManager.trajectoryManager(vp)
+tm.load_csv(testLocationStr)
+tm._compute_geometry()
+print(tm._compute_speed_profile())
